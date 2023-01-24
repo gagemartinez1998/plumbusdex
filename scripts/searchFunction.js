21891e4121fda3dd76ba00b96751ae9c0b706c86
Shirley Nguyen <shirley.nguyen8@outlook.com>
2023-01-23 22:01:08 -0600

    Added search and filter functions

--- scripts/searchFunction.js
+++ scripts/searchFunction.js
@@ -1,3 +1,17 @@
 const searchCards = () => {
-    
+    var input, filter, cards, cardContainer, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("card-container");
+    cards = cardContainer.getElementsByClassName("card");
+
+    for (let i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-title");
+        console.log(title);
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
 }
