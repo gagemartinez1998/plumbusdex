122f1e932038a9bef3b2a8cdc7c16288d248f61c
Shirley Nguyen <shirley.nguyen8@outlook.com>
2023-01-25 21:15:42 -0600

    Added Enter eventListener and CSS Styling

--- scripts/searchFunction.js
+++ scripts/searchFunction.js
@@ -7,11 +7,38 @@ const searchCards = () => {
 
     for (let i = 0; i < cards.length; i++) {
         title = cards[i].querySelector(".card-title");
-        console.log(title);
+        // console.log(title);
         if (title.innerText.toUpperCase().indexOf(filter) > -1) {
             cards[i].style.display = "";
         } else {
             cards[i].style.display = "none";
         }
     }
+
+    // input.addEventListener("keyup", function(event) {
+    //     if (event.key === "Enter") {
+    //         event.preventDefault();
+    //         document.getElementById("searchBtn").click();
+    //     }
+    // })
 }
+
+// const enterSubmit = () => {
+
+//     var input = document.getElementById("myFilter")
+    
+//     input.addEventListener("keypress", function(event) {
+//             if (event.key === "Enter") {
+//                 event.preventDefault();
+//                 document.getElementById("searchBtn").click();
+//             }
+//         })
+
+//     // $("#myFilter").keyup(function(event) {
+//     //     if (event.key === "Enter") {
+//     //         event.preventDefault();
+//     //         $("#searchBtn").click();
+//     //             }
+//     // });
+//     }
+    
